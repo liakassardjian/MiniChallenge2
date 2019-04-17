0e3925b6998b742f1c9147a100821d32a1573e74
felipe Petersen <felipekpetersen@gmail.com>
2019-04-17 17:49:43 -0300

    rounded corners

--- CorAcessivel/CorAcessivel/ViewController.swift
+++ CorAcessivel/CorAcessivel/ViewController.swift
@@ -12,15 +12,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var selectImageView: UIImageView!
     @IBOutlet weak var corMediaStackView: UIStackView!
-    @IBOutlet weak var corMediaView: UIView!
+    @IBOutlet weak var corMediaView: ColorView!
+    @IBOutlet weak var harmonizacaoStackView: UIStackView!
+    @IBOutlet weak var harmonizacaoTextField: UITextField!
+    @IBOutlet weak var corHarmonizacao1View: ColorView!
+    @IBOutlet weak var corHarmonizacao2View: ColorView!
+    @IBOutlet weak var corHarmonizacao3View: ColorView!
+    @IBOutlet weak var daltonismoStackView: UIStackView!
+    @IBOutlet weak var daltonismoTextField: UITextField!
+    @IBOutlet weak var corDaltonico1View: ColorView!
+    @IBOutlet weak var corDaltonico2View: ColorView!
+    @IBOutlet weak var corDaltonico3View: ColorView!
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
