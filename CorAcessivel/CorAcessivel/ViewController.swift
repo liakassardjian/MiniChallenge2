b18c70736072f7496d8a1072608e2c6fe7856452
felipe Petersen <felipekpetersen@gmail.com>
2019-04-17 17:16:21 -0300

    layout

---
+++ CorAcessivel/CorAcessivel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CorAcessivel
+//
+//  Created by Felipe Kaça Petersen on 17/04/19.
+//  Copyright © 2019 Felipe Kaça Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var selectImageView: UIImageView!
+    @IBOutlet weak var corMediaStackView: UIStackView!
+    @IBOutlet weak var corMediaView: UIView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
